a274b42f8d1200448f3a9d4fc2a1f9aa27780f92
tyysun <tangyysun@gmail.com>
2018-02-05 19:57:39 +0800

    更换协议

--- song.js
+++ song.js
@@ -22,10 +22,18 @@ $(
       })
     })
     let audio = document.createElement('audio')
-    audio.src = 'http://dl.stream.qqmusic.qq.com/C400001J5QJL1pRQYB.m4a?fromtag=38&vkey=F39DE47EA75E737F5F2C9D19FE80DF87EC7CB1EA96740CEB7F850AC61034274903B9FBC4343A34359BFA2ECC92305FF5418D5A1C8A4283FB&guid=6800799492'
+    audio.src = '//dl.stream.qqmusic.qq.com/C400001J5QJL1pRQYB.m4a?fromtag=38&vkey=A296ECD23A31913BD0F6D990CAF55DE1577D9B44087923DEFCEEBFFACE53DBEE7C5B5BD48F0EA9C869D9EA8A8D859DB25E5ACC4436A64E31&guid=6800799492'
     audio.oncanplay = function(){
       audio.play()
       $('.disc-container').addClass('playing')
     }
+    $('.icon-pause').on('click',function(){
+      audio.pause()
+      $('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click',function(){
+      audio.play()
+      $('.disc-container').addClass('playing')
+    })
   }
 )
